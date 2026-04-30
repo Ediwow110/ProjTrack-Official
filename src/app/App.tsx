f2c9601a0a937c039321b8dab179f9be1a3175bc
Ediwow110 <YOUR_EDIWOW110_EMAIL>
2026-04-30 16:13:45 +0800

    Fix blank white page: Correct React Router v7 imports and remove untracked BrandingProvider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,15 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { ThemeProvider } from "./context/ThemeContext";
 import { Toaster } from "sonner";
+import { BrandingProvider } from "./components/brand/BrandingProvider";
 
 export default function App() {
   return (
     <ThemeProvider>
-      <RouterProvider router={router} />
-      <Toaster richColors position="top-right" />
+      <BrandingProvider>
+        <RouterProvider router={router} />
+        <Toaster richColors position="top-right" />
+      </BrandingProvider>
     </ThemeProvider>
   );
 }
