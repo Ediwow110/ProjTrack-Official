1da302f8ce5fcebb23e555767dd7fe0eacb72a37
JANELLA VALDEZ <128321849+Ediwow110@users.noreply.github.com>
2026-05-14 06:47:24 +0800

    test: add security jest config

---
+++ backend/jest.security.config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  testMatch: ['<rootDir>/test/security/**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          module: 'commonjs',
+          target: 'es2019',
+          experimentalDecorators: true,
+          emitDecoratorMetadata: true,
+          allowSyntheticDefaultImports: true,
+          esModuleInterop: true,
+          strict: false,
+          skipLibCheck: true,
+          rootDir: '.',
+        },
+      },
+    ],
+  },
+  clearMocks: true,
+};
